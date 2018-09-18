e5975ca8e75beda7577d257c13184691347bbd78
zhangdong945 <zhangdong945@hotmail.com>
2018-09-18 13:45:31 +0800

    2nd

---
+++ testgit2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testgit2
+{
+    class Program
+    {
+        static int Main()
+        {
+            Console.WriteLine("Hello world!");
+            return 0;
+        }
+    }
+}
